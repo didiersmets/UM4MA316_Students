0a03ab2bd17c3e37c75db8209ea7b7359f5d34b1
Adrien Evo <adrien.evo@protonmail.com>
2026-09-13 17:38:26 +0200

    read_file.c

---
+++ TP0/src/read_file.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc!=2){
+        fprintf(stderr, "%s <file path>\n", argv[0]);
+        return 1;
+    }
+    FILE* fp;
+    fp = fopen(argv[1], "r");
+    size_t n = 1024;
+    char buff[n];
+    char *c;
+
+    while ((c=fgets(buff, n,fp)) != NULL) {
+        printf("%s", c);
+    }
+}
