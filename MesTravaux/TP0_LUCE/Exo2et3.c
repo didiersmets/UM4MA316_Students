c4b0c88c3844fafdaba1eba8528d8bd5b8ebf66d
FlorianLuce <guiguiluce@gmail.com>
2026-09-14 15:49:19 +0200

    lundi14

--- MesTravaux/TP0_LUCE/Exo2et3.c
+++ MesTravaux/TP0_LUCE/Exo2et3.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int recursive(int i, int j){
-    if ( j==0 || j>=i){
+    //retroune la combinaison de j parmi i
+    if ( j==0 || j>=i){ // 0 parmi n = 1
         return 1;
     } else {
-        return recursive(i-1,j-1)+recursive(i-1,j);
+        return recursive(i-1,j-1)+recursive(i-1,j); //car j-1 parmi i-1 + j parmi i-1 fait j parmi i
     }
 }
 
@@ -14,7 +15,7 @@ void pascal_triangle(int n){
 
     for(int i=0; i<n ; i++){
         for(int j=0; j<=i ; j++){
-            printf("%d", recursive(i,j));
+            printf("%d ", recursive(i,j));
         }
         printf("\n");
     }
