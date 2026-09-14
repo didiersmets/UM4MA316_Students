b6cca57ce7ecea3a21f11d81ae54784d0b25d314
fabioladearizon <21601013@401-13.math.upmc.fr>
2026-09-14 15:48:03 +0200

    hello et avancement pascal

---
+++ TP0/pascal.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+int main (int argc, char ** argv)
+{
+    int n = atoi(argv)
+    void*malloc(size_t, Size)
+
+    int*A = malloc(n*n*sizeof(int))
+
+    for (i= 0; i<n ; i++) {
+        for (j=0 ; j<=i ; j++) {
+            if (j>o ; j<i){
+                A[i*n+j] = A[(i-1)*n+(j-1)] + A[(i-1)*n +j]
+            }
+               
+            else A[i*n+j]
+
+
+            printf("A, %s\n", argv[1]);
+        }
+    
+    }
+    
+    free(A);
+
+}
+
