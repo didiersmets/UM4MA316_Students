a84fa593b5e7c17783e3334356ec49056e2c6ba6
titocapovilla <titocaput@gmail.com>
2026-09-18 15:08:56 +0200

    implemented queue, to be tested extensively

--- TP1/src/circular_buffer_queue.c
+++ TP1/src/circular_buffer_queue.c
@@ -1,3 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include  <string.h>
+
 #include "../include/circular_buffer_queue.h"
 
 // -- Implementation of the functions in header file circular_buffer_queue.h -- 
@@ -41,11 +45,8 @@ void queue_enqueue(struct Queue *q, const void *src){
 
     if(q->length == q->capacity){
         //handle the fact that you finished space
+        enlarge_queue_capacity(q);
     }
-
-    q->length++;
-    q->front++;
-
     // array index to be accessed
     int index = (q->front + q->length) % q->capacity;
 
@@ -54,16 +55,57 @@ void queue_enqueue(struct Queue *q, const void *src){
 
     // calculated address and memcpy. Cast to char is just to the sum sums 1 byte * byte_offset
     void* element_ptr = (char*)q->data + byte_offset;
+    //copy the new item 
     memcpy(element_ptr, src, q->elem_size);
+    //increase the length
     q->length++;
-
 }
 
 void queue_dequeue(struct Queue *q, void *dest){
-
+    if(q->length == 0){ //the queue is already empty
+        printf("Dequeue was called althought the queue is empty");
+    }else{
+        q->length--;
+        //move the front by 1 address
+        //if the front has reached capacity make it 0
+        q->front++;
+        if (q->front == q->capacity){
+            q->front == 0;
+        }
+    }
 }
 
 static void enlarge_queue_capacity(struct Queue * q){
+    //allocate a new queue to copy stuff to
+    struct Queue *new_q = malloc(sizeof(struct Queue));
+
+    int new_capacity = q->capacity +1 ;
+
+    new_q->front = 0;
+    new_q->length = q->length;
+    new_q->capacity = new_capacity;
+    new_q->elem_size = q->elem_size;
+
+    void *new_data = malloc(new_capacity * q->elem_size);
+    new_q->data = new_data;
 
 
+    //transfer the old data into new data.
+    for(int e = 0; e<q->length; e++){
+        // array index to be accessed
+        int index_q = (q->front + e) % q->capacity;
+        int index_new_q = (new_q->front + e) % new_q->capacity;
+
+        //conversion to byte offset
+        size_t byte_offset_q = (size_t)index_q * q->elem_size;
+        size_t byte_offset_new_q = (size_t)index_new_q * new_q->elem_size;
+
+        // calculated address and memcpy. Cast to char is just to the sum sums 1 byte * byte_offset
+        void* element_ptr_q = (char*)q->data + byte_offset_q;
+        void* element_ptr_new_q = (char*)new_q->data + byte_offset_new_q;
+
+        //copy the new item 
+        memcpy(element_ptr_q, element_ptr_new_q, new_q->elem_size);
+
+    }
 }
--- TP1/src/test_queue.c
+++ TP1/src/test_queue.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/circular_buffer_queue.h"
+
 
 int main(int argc, char* argv[]){
 
@@ -8,7 +10,30 @@ int main(int argc, char* argv[]){
         printf("Please only provide one interger as input \n");
         return 1;
     }else{
+    
+        int n = atoi(argv[1]);
+        printf("Your input no is: %d \n", n);
 
+        //First initiate an empty queue q of integers,
+        struct Queue *q = queue_init(sizeof(int),n);
+
+
+        //iteratively pick random integers p
+        //and then enqueue p in q if p is even
+        //and instead dequeue one item from q whenp is odd.
+        for(int i = 0; i<n; i++){
+            int p = rand() % 101; //generate random between 0 and 100;
+            if(p % 2 == 0){
+                queue_enqueue(q, &p);
+            }else{
+                int dest;
+                queue_dequeue(q, &dest);
+
+            }
+            //the main function should return the largest length l_max
+            //of the queue q during the whole process
+            printf("Current q length: %d \n",q->length);
+        }
     }
     return 0;
 }
