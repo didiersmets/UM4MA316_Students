2ca96ab77660b5edae5498b064fca2160aeb4560
fabioladearizon <21601013@401-13.math.upmc.fr>
2026-09-21 15:46:20 +0200

    TP1 21-09

---
+++ TP1/include/src/test_queue.c
@@ -0,0 +1,18 @@
+#include <stdio.>
+#include <stdlib.> 
+#include <circular_buffer_queue.h>
+
+int main(int argc, char ** argv);
+    int n = atoi(argv[1])
+
+    q = queue_init(sizeof)
+
+    p = rand
+    if (p % 2 == 0) {
+        queue_enqueue()
+    }
+    else (queue_dequeue())
+
+    l_max
+
+    if q->length > l
