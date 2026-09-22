5dd1b70d5921af268ecc1e37efa0afb7015cfe30
carlobriscolini <carlobrisko@gmail.com>
2026-09-22 17:24:09 +0200

    multiple arrays to sort

--- TP1/src/test_sorting.c
+++ TP1/src/test_sorting.c
@@ -9,9 +9,14 @@ int main(int argc, char *argv[]) {
 
     srand(time(NULL));
 
-    int N[1] = {5};
+    int N[7] = {10, 20, 50, 100, 200, 500, 1000};
 
     for(int i = 0; i < sizeof(N) / sizeof(int); i++) {
+
+        printf("\n\
+------------------------------------------------\n\
+sorting array of length %d\n\
+------------------------------------------------\n", N[i]);
 
         int *arr = malloc(N[i] * sizeof(int));
         int *arr1 = malloc(N[i] * sizeof(int));
@@ -32,46 +37,30 @@ int main(int argc, char *argv[]) {
 
         // bubble sort
 
-        for(int j = 0; j < N[i]; j++) {
-            bubbleSort(arr, N[i]);
-        }
-
-        printf("sorted array using bubble sort:\n");
-
-        for(int j = 0; j < N[i]; j++) {
-            printf("%d ", arr[j]);
-        }
-
-        printf("\n");
+        bubbleSort(arr, N[i]);
 
         // insertion sort
 
-        printf("sorted array using insertion sort:\n");
-
-        for(int j = 0; j < N[i]; j++) {
-            insertionSort(arr1, N[i]);
-        }
-
-        for(int j = 0; j < N[i]; j++) {
-            printf("%d ", arr1[j]);
-        }
-
-        printf("\n");
+        insertionSort(arr1, N[i]);
 
         // merge sort
 
-        printf("sorted array using merge sort:\n");
+        mergeSort(arr2, 0, N[i] - 1);
+
+        // sorted array and check that the sorting is the same using all three algorithms
 
         for(int j = 0; j < N[i]; j++) {
-            mergeSort(arr2, 0, N[i] - 1);
+            if(arr[j] != arr1[j] || arr1[j] != arr2[j]) {
+                printf("different sorting resulted from the algorithms\n");
+                return 1;
+            }
         }
 
+        printf("sorted array:\n");
         for(int j = 0; j < N[i]; j++) {
-            printf("%d ", arr2[j]);
+            printf("%d ", arr[j]);
         }
-
         printf("\n");
-
         
     }
 
