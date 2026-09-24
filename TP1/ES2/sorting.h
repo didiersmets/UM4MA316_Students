ce01836188400de088309afee3ca39a0ed2bd163
luca-grecchi <lucaa.grecchi@gmail.com>
2026-09-24 15:21:16 +0200

    implemented ping pong merge sort

--- TP1/ES2/sorting.h
+++ TP1/ES2/sorting.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 void bubbleSort(int* arr, int n){
     for(int j=0;j<n;j++){
@@ -32,17 +33,18 @@ void insertionSort(int* arr, int n){
 
 void merge(int* T, int p, int q, int r){
     int *S = malloc((r+1-p)*sizeof(int));
+    int idx1 = p;
+    int mid = q;
+    int idx2 = q+1;
 
-    for(int i=0;i<r-p;i++){
-        if(T[p]<T[q+1]){
-            S[i] = T[p];
-            p++;
+    for(int i=0;i<=r-p;i++){
+        if(idx2>r || (idx1<=mid && T[idx1]<=T[idx2])){
+            S[i] = T[idx1++];
         }else{
-            S[i] = T[q+1];
-            q++;
+            S[i] = T[idx2++];
         }
     }
-    memcpy(T+p*sizeof(int),S,(r+1-p)*sizeof(int));
+    memcpy(&T[p],S,(r+1-p)*sizeof(int));
     free(S);
 }
 
@@ -53,4 +55,38 @@ void mergeSort(int* arr, int p, int r){
         mergeSort(arr, q+1, r);
         merge(arr, p, q, r);
     }
+}
+
+
+//MergeSort faster
+void mergeR(int* unsorted, int p, int q, int r, int* sorted){
+    int idx1 = p;
+    int mid = q;
+    int idx2 = q+1;
+
+    for(int i=0;i<=r-p;i++){
+        if(idx2>r || (idx1<=mid && unsorted[idx1]<=unsorted[idx2])){
+            sorted[i+p] = unsorted[idx1++];
+        }else{
+            sorted[i+p] = unsorted[idx2++];
+        }
+    }
+}
+
+void mergeSortRecursive(int* unsorted, int p, int r, int* sorted){
+    if(p<r){
+        int q = (p+r)/2;
+        mergeSortRecursive(sorted,p,q,unsorted);
+        mergeSortRecursive(sorted,q+1,r,unsorted);
+        mergeR(unsorted,p,q,r,sorted);
+    }else if(p==r){
+        sorted[p] = unsorted[p];
+    }
+}
+
+void mergeSortUser(int *A, int n){
+    int* B = malloc(n*sizeof(int));
+    memcpy(B,A,n*sizeof(int));
+    mergeSortRecursive(B,0,n-1,A);
+    free(B);
 }
--- TP1/ES2/test_sorting.c
+++ TP1/ES2/test_sorting.c
@@ -1,30 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "sorting.h"
+#include <time.h>
 
 int main(int argc, char* argv[]){
-
     /*
-   int N[] = {10, 20, 50, 100, 200, 500, 1000};
+    int N[] = {10, 20, 50, 100, 200, 500, 1000};
+
+    srand(time(NULL));
+
+    FILE *fptr;
+    fptr = fopen("Time_rep.txt", "w");
 
     for(int i=0;i<7;i++){
+        fprintf(fptr, "\n\n\nArray size: %d\n", N[i]);
+        int *base_arr = malloc(N[i]*sizeof(int));
         int *arr = malloc(N[i]*sizeof(int));
 
-        for(int j=0;j<N[i];j++){
-            int val = rand();
-            arr[j] = val;
-        }
-        
-        bubbleSort(arr,N[i]);
-
         for(int k=0;k<N[i];k++){
-            printf("%d ", arr[k]);
+            int val = rand() % 2000;
+            base_arr[k] = val;
         }
 
+        for(int j=0;j<3;j++){
+
+            if(j==0){
+                fprintf(fptr, "\n\nBubble Sort\n");
+            }else if(j==1){
+                fprintf(fptr, "\n\nInsertion Sort\n");
+            }else{
+                fprintf(fptr, "\n\nMerge Sort\n");
+            }
+
+            memcpy(arr, base_arr, N[i]*sizeof(int));
+
+            /*
+            for(int k=0;k<N[i];k++){
+                printf("%d ", arr[k]);
+            }
+            printf("\n\n");
+            *
+
+            clock_t start = clock();
+            if(j==0){
+                bubbleSort(arr,N[i]);
+            }else if(j==1){
+                insertionSort(arr,N[i]);
+            }else{
+                mergeSort(arr,0,N[i]-1);
+            }
+
+            clock_t end = clock();
+            double time = (double)(end-start)/CLOCKS_PER_SEC;
+            fprintf(fptr, "Execution time: %2f\n", time);
+
+            /*
+            for(int k=0;k<N[i];k++){
+                printf("%d ", arr[k]);
+            }
+            printf("\n\n\n");
+            *
+        }
+
+        free(base_arr);
         free(arr);
     }
     */
 
+    
     int arr[]  = {11,54,23,6,7,89,4,2,8,45};
 
     for(int k=0;k<10;k++){
@@ -34,12 +78,14 @@ int main(int argc, char* argv[]){
 
     //bubbleSort(arr, 10);
     //insertionSort(arr,10);
-    mergeSort(arr, 0, 10-1);
+    //mergeSort(arr, 0, 9);
+    mergeSortUser(arr,10);
 
     for(int k=0;k<10;k++){
         printf("%d ", arr[k]);
     }
     
 
+    //fclose(fptr);
     return 0;
 }
